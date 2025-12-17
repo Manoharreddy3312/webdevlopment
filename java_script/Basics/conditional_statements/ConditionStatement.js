1e59cc23784fea603218fcec9b8d7a9229714548
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2025-12-17 13:05:48 +0530

    commit

---
+++ java_script/Basics/conditional_statements/ConditionStatement.js
@@ -0,0 +1,79 @@
+// ! if , else if statements
+
+// let marks = 63;
+// if (marks > 90 && marks <= 100) {
+//     console.log("grade is A+");
+// }
+
+// else if (marks > 80 && marks <= 90) {
+//     console.log("grade is A");
+// }
+
+// else if (marks > 60 && marks <= 80) {
+//     console.log("grade is B");
+// }
+
+// else if (marks > 40 && marks <= 60) {
+//     console.log("grade is PASS");
+// }
+// else if (marks >= 0 && marks < 40) {
+//     console.log("Fail");
+
+// }
+// else {
+//     console.log("Enterd valid marks");
+// }
+// =====================================================================
+//! switch case
+
+// let day = "thusday";
+// switch (day) {
+//     case "monday":
+//         console.log("JAVA & SQL");
+//         break;
+//     case "tuesday":
+//         console.log("JAVA & WEBDEVELOPMENT");
+//         break;
+//     case "wednesday":
+//         console.log("PYTHON & WEBDEVELOPMENT");
+//         break;
+//     case "thusday":
+//         console.log("SQL & WEBDEVELOPMENT");
+//         break;
+//     case "friday":
+//         console.log("JAVA & WEBDEVELOPMENT");
+//         break;
+//     case "saturday":
+//         console.log("MOCK INTERVIEW");
+//         break;
+
+//     case "sunday":
+//         console.log("HOLIDAY");
+//         break;
+
+//     default:
+//         console.log("enter a valid day");
+
+// }
+// =====================================================================
+
+// let marks = 22;
+// switch (true) {
+//     case marks > 80 && marks <= 100:
+//         console.log("A");
+//         break;
+//     case marks > 60 && marks <= 80:
+//         console.log("B");
+//         break;
+//     case marks > 40 && marks <= 60:
+//         console.log("PASS");
+//         break;
+//     case marks >= 0 && marks < 40:
+//         console.log("FAIL");
+//         break;
+//     default:
+//         console.log("enter valid marks");
+//         break;
+// }
+
+// =====================================================================
---
+++ java_script/Basics/function/function.js
@@ -0,0 +1,33 @@
+//! NAMED FUNCTION
+
+// function add(){
+//     console.log("im a function");
+    
+// }
+
+// add();
+
+// ! ANONYMOUS FUNCTION
+
+// function (){
+//     console.log("anonymous funcrtion");
+    
+// }
+
+// ! FUNCTION WITH EXPRESSION
+
+// let a = function(){
+//     console.log("FUNCTION WITH EXPRESSION");
+    
+// }
+// a();
+
+let a = function demo(){
+    console.log("FUNCTION WITH EXPRESSION-2");
+    
+}
+a();
+
+
+
+
