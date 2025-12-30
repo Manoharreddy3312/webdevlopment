252e41e48728ceee76924d5dfb8bae731c3124b0
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2025-12-30 11:43:32 +0530

    commit

--- java_script/Basics/object/object.js
+++ java_script/Basics/object/object.js
@@ -198,34 +198,32 @@
 //     console.log(i.rating.rate);
 // })
 // ==================================================================
-let user = [{
-    name:"anish",
-    active:false
-},
-{
-    name : "ravi",
-    active:true
-},
-{
-    name : "sweety",
-    active:true
-}
-]
+// let user = [{
+//     name:"anish",
+//     active:false
+// },
+// {
+//     name : "ravi",
+//     active:true
+// },
+// {
+//     name : "sweety",
+//     active:true
+// }
+// ]
 
 // for(let i=0; i<user.length; i++){
 //     if(user[i].active === true){
 //         console.log(user[i]);
 //         break;
 //     }
-
+// }
 
 // let a= user.find((v)=>{
-//     return v.active==true
+//     return v.active==true;
 // })
 
 // console.log(a);
-
-
 
 
 //! let arr = [2,5,3]. find the product of all numbers using array method
@@ -237,13 +235,54 @@ let user = [{
 // console.log(product);
 
 
+// =================================================================================
+
+//! ---------------USING OBJECT CONCTRUCTOR--------------------------
+
+let emp = new Object();
+console.log(emp);
+emp.name = "Kl Rahul";
+emp.age = 31
+console.log(emp);
 
 
+// =================================================================================
+
+//! ---------------USING FUNCTION AS A CONSTRUCTOR-------------------
 
 
+function Cake(cname,cprice){
+    this.cake_name = cname;
+    this.cake_price = cprice;
+}
+let c1 = new Cake("white Forest",1000)
+console.log(c1);
 
+let c2 = new Cake("Red velvate",1500)
+console.log(c2);
 
+let c3 = new Cake("Honey Cake",1700)
+console.log(c3);
 
+// =================================================================================
+
+// ! math(): The Math object provides properties and methods for mathematical constants and functions.
+
+// console.log(Math.PI); //! 1
+
+// let a = Math.floor(20.2) //! 2
+// let a = Math.ceil(20.2)  //! 3
+// let a = Math.round(20.2)  //! 4
+// let a = Math.random()  //! 5
+
+// (let a = Math.random()*1000)  //!generate random numbers like OTP
+// let a = Math.min(9,7,3,7,56)  //! 6
+// let a = Math.max(9,7,3,7,56)  //! 7
+// let a = Math.sqrt(4)
+// let a = Math.cbrt(27)
+// let a = Math.pow(5,2)
+let a = Math.pow(5,2)
+console.log(a);
 
 
 
