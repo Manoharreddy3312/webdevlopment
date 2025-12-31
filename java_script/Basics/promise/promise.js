7a767ee41adc4ea8706ea1d95bad25d8a188a14e
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2025-12-31 11:19:24 +0530

    commit

---
+++ java_script/Basics/promise/promise.js
@@ -0,0 +1,13 @@
+// console.log("hellow");
+
+// ! PROMISE():
+// A Promise is an object representing the eventual completion or failure of an asynchronous operation.
+// It has three states: pending, fulfilled, and rejected.
+// Promises are used to handle asynchronous operations in JavaScript.
+
+// OR
+
+// IT IS USED TO PERFORM ASYNCHRONIUS TASK/OPERATION IN JS
+// A Promise is an object representing the eventual completion or failure of an asynchronous operation.
+
+
