fdc5a176d6b87d7ce8d7ede1133df2f01a04c1a2
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2026-01-02 11:43:13 +0530

    commit

--- java_script/Basics/promise/promise.js
+++ java_script/Basics/promise/promise.js
@@ -7,29 +7,198 @@
 
 // OR
 
-// IT IS USED TO PERFORM ASYNCHRONIUS TASK/OPERATION IN JS
+// IT IS USED TO PERFORM ASYNCHRONUS TASK/OPERATION IN JavaScript.
 // A Promise is an object representing the eventual completion or failure of an asynchronous operation.
 
 
 
+// let p1 = new Promise((req, rej) => {
+//     req("i am resolved")
+//     rej("i am rejected")
+// })
+
+// p1.then(() => {
+//     console.log("Resolved then");
+// })
+
+// p1.catch(() => {
+//     console.log("rejected catch");
+
+// })
+// p1.finally(() => {
+//     console.log("i am finally block");
+
+// })
+// ===================
+// p1.then(()=>{
+// console.log(v);
+//     console.log("i am resolved");
+// }).catch(()=>{
+//     console.log("catch rejected");
+// }).finally(()=>{
+//     console.log("finally block");
+// })
+
+// ==================================================
+// ! TYPES OF PROMUISE:
+
+// 1.all
+// 2.allSettled
+// 3.race
+// 4.any
+//! 1.all:
+// let p1 = new Promise((res, rej) => {
+//     res("I am reolved..")
+// })
+
+// let p2 = new Promise((res, rej) => {
+//     res("I am res---p2")
+// })
+// let p3 = new Promise((res, rej) => {
+//     res("I am p3---res")
+// })
+
+// Promise.all([p1, p2, p3]).then((v) => {
+//     console.log(v);
+//     console.log("then block");
+// }).catch(() => {
+//     console.log("catch block");
+// }).finally(() => {
+//     console.log("finally block");
+// })
+
+// ==================================================
+//! 2.allSettled:
+
+// let p1 = new Promise((res, rej) => {
+//     res("I am reolved..")
+// })
+
+// let p2 = new Promise((res, rej) => {
+//     res("I am res---p2")
+// })
+// let p3 = new Promise((res, rej) => {
+//     res("I am p3---res")
+// })
+
+// Promise.allSettled([p1, p2, p3]).then((v) => {
+//     console.log(v);
+//     console.log("then block");
+// }).catch(() => {
+//     console.log("catch block");
+// }).finally(() => {
+//     console.log("finally block");
+// })
+
+// ==================================================
+//! 3.race:
+
+// let p1 = new Promise((res, rej) => {
+//     res("I am reolved..")
+// })
+
+// let p2 = new Promise((res, rej) => {
+//     res("I am res---p2")
+// })
+// let p3 = new Promise((res, rej) => {
+//     res("I am p3---res")
+// })
+
+// Promise.race([p1, p2, p3]).then((v) => {
+//     console.log(v);
+//     console.log("then block");
+// }).catch(() => {
+//     console.log("catch block");
+// }).finally(() => {
+//     console.log("finally block");
+// })
+
+// ================
+
+// setTimeout(()=>{
+//     console.log("Happy new Year 2026");
+// },5000)
+
+// setInterval(()=>{
+//     console.log("Happy new Year 2026");
+// },5000)
+// ================
+
+// let p1 = new Promise((res, rej) => {
+//     setTimeout(() => {
+//        res("i am --p1");
+//     }, 4000)
+// })
+// let p2 = new Promise((res, rej) => {
+//     setTimeout(() => {
+//        res("i am --p2");
+//     }, 3000)
+// })
+// let p3 = new Promise((res, rej) => {
+//     setTimeout(() => {
+//        res("i am --p3");
+//     }, 2000)
+// })
+// Promise.race([p1, p2, p3]).then((v) => {
+//     console.log(v);
+//     console.log("then block");
+// }).catch(() => {
+//     console.log("catch block");
+// }).finally(() => {
+//     console.log("finally block");
+// })
 
 
 
+// ==================================================
+//! 4.any:
 
 
+// let p1 = new Promise((res, rej) => {
+//     rej("I am reolved..")
+// })
 
+// let p2 = new Promise((res, rej) => {
+//     rej("I am ---p2")
+// })
+// let p3 = new Promise((res, rej) => {
+//     rej("I am p3")
+// })
 
+// Promise.any([p1, p2, p3]).then((v) => {
+//     console.log(v);
+//     console.log("then block");
+// }).catch(() => {
+//     console.log("catch block");
+// }).finally(() => {
+//     console.log("finally block");
+// })
 
+// ==================
 
-
-
-
-
-
-
-
-
-
+let p1 = new Promise((res, rej) => {
+    setTimeout(() => {
+       rej("i am --p1");
+    }, 4000)
+})
+let p2 = new Promise((res, rej) => {
+    setTimeout(() => {
+       rej("i am --p2");
+    }, 3000)
+})
+let p3 = new Promise((res, rej) => {
+    setTimeout(() => {
+       rej("i am --p3");
+    }, 2000)
+})
+Promise.any([p1, p2, p3]).then((v) => {
+    console.log(v);
+    console.log("then block");
+}).catch(() => {
+    console.log("catch block");
+}).finally(() => {
+    console.log("finally block");
+})
 
 
 
