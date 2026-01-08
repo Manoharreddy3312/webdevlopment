a896d2cad79839f1afdf9ead10382af2753f13d7
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2026-01-09 00:00:46 +0530

    commit

--- java_script/Basics/fetch/fetchData.js
+++ java_script/Basics/fetch/fetchData.js
@@ -1,38 +1,60 @@
-// !FETCH:
-// =======
-// it IS THE PROMISE BASED INBUILT METHOD WHICH IS USED TO GET THE DATA FROM THE SERVER.(backend)
+// // !FETCH:
+// // =======
+// // it IS THE PROMISE BASED INBUILT METHOD WHICH IS USED TO GET THE DATA FROM THE SERVER.(backend)
 
-fetch("https://dummyjson.com/recipes").then((res) => {
-    return res.json()
-}).then((val) => {
-    console.log(val.recipes);
-    let output = ""
-    val.recipes.map((v) => {
-        output += `
-        <main>
-        <img src = "${v.image}"/>
-        <h2>${v.name}</h2>
-        <h3>servings: ${v.servings}</h3>
-        <ul>
-        ${v.mealType.map((meal) => `<li>${meal}</li>`).join("")}
-        cuisine: ${v.cuisine}
+// fetch("https://dummyjson.com/recipes").then((res) => {
+//     return res.json()
+// }).then((val) => {
+//     console.log(val.recipes);
+//     let output = ""
+//     val.recipes.map((v) => {
+//         output += `
+//         <main>
+//         <img src = "${v.image}"/>
+//         <h2>${v.name}</h2>
+//         <h3>servings: ${v.servings}</h3>
+//         <ul>
+//         ${v.mealType.map((meal) => `<li>${meal}</li>`).join("")}
+//         cuisine: ${v.cuisine}
 
-        </ul>
-        <h4 class="tags">Tags:</h4>
-        <ul>
-        ${v.tags.map((tag) => `<li>${tag}</li>`).join("")}
-        </ul>
-        <h3>rating: ${v.rating} ⭐</h3>
+//         </ul>
+//         <h4 class="tags">Tags:</h4>
+//         <ul>
+//         ${v.tags.map((tag) => `<li>${tag}</li>`).join("")}
+//         </ul>
+//         <h3>rating: ${v.rating} ⭐</h3>
 
 
 
-        </main>
-        `
-    })
-    document.getElementById("container").innerHTML = output
-}).catch((err) => {
-    console.log(err.message);
-    document.getElementById("container").innerHTML = `<h2 style="color:red;">${err.message}</h2>`
+//         </main>
+//         `
+//     })
+//     document.getElementById("container").innerHTML = output
+// }).catch((err) => {
+//     console.log(err.message);
+//     document.getElementById("container").innerHTML = `<h2 style="color:red;">${err.message}</h2>`
 
-})
+// })
 
+async function loadRecipe(query = "") {
+    let response = await fetch(`https://dummyjson.com/recipes/search?q=${query}`);
+
+    let data = await response.json();
+    let innerHTML = "";
+    data.recipes.forEach((recipe) => {
+        innerHTML += `<div class="recipe-card">
+                        <img src="${recipe.image}"/>
+                      <h2>${recipe.name}</h2>
+                        
+                     </div>`;
+    });
+    document.getElementById("recipes-container").innerHTML = innerHTML;
+}
+
+const searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", () => {
+    const query = document.getElementById("search-input").value.trim();
+    loadRecipe(query);
+});
+
+loadRecipe();
