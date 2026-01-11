812f95f64ef711bf8139be57a0b8304e181d76f6
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2026-01-11 11:32:49 +0530

    first commit

--- java_script/Basics/BOM/bom.js
+++ java_script/Basics/BOM/bom.js
@@ -4,7 +4,7 @@
 // IT IS USED BY JAVASCRIPT TO INTERACT WITH HTML ELEMENTS/BROWSER
 // WINDOW IS TOP-LEVEL OBJECT
 
-// 1.POP-UP METHODS:
+//! 1.WINDOW :
 
 // open()        : opens any document in new window
 // close()        : closes current window
@@ -13,7 +13,9 @@
 // prompt()        : pops up input box
 // confirm()        : pops up confirm box with ok, cancel.
 
-// 2. window(SCREEN)
+// =============================================================================
+
+//! 2.Navigaton: it s used to extra information about browser
 
 // appName        : It returns browser family name. [Netscape]
 // appVersion    : Browser version
@@ -22,15 +24,86 @@
 // language        : Current browser language.
 // cookieEnabled    : Return cookie status.
 // online        : It returns current connection status
-//                 [online or offline
+//                 [online or offline]
 
-// 3. navigator
-// 4. location
-// 5. history
-// 6. document
-    
-// 1.POP-UP METHODS : 
+// =============================================================================
 
-// GENERATE 6- DEGIT OTP 
+//! 3. LOCATION:
+// host        : server name / IP address
+// port        : returns port number
+// protocol        : returns protocol [http, ftp]
+// href        : gets and set url.
+// pathname        : folder and file name.
+// search        : gets query string.
+// hash        : returns hash location  [ # ]
 
-// javascript
+// =============================================================================
+
+//! 4.SCREEN:
+// screen.width
+// screen.height
+// screen.availWidth
+// screen.availHeight
+// screen.colorDepth
+// screen.pixelDepth
+console.log(screen.height)
+console.log(screen.availWidth)
+console.log(screen.availHeight)
+console.log(screen.colorDepth)
+console.log(screen.pixelDepth)
+
+// =============================================================================
+
+//! 5. history
+
+// length        : returns the total count of pages in
+//                  current browsing history.
+
+// back()        : moves to one page back in browsing
+//                   history.
+
+// forward()     : moves to one page forward.
+
+// go()          : moves to specific page, by page               name or interger value.
+
+// =============================================================================
+
+//! 6.console:
+
+// console.log()
+// console.error()
+// console.warth()
+// console.table()
+
+// =============================================================================
+
+//! 7. document:
+
+// - It provides the properties and methods that are used to control document elements.
+//     document.write()
+//     document.writeln()
+//     document.getElementById()
+//     document.querySelector() etc..
+
+// =============================================================================
+
+// GENERATE 6- DEGIT OTP
+
+// let otp = Math.floor(100000 + Math.random() * 900000);
+// console.log(otp);
+// alert(otp);
+
+// let userInput = prompt("Enter OTP");
+
+// if (userInput == otp) {
+    //     console.log("Your otp is correct");
+    //     alert("Your otp is correct");
+    // } else {
+        //     console.log("Your otp is wrong");
+//     alert("Your otp is wrong");
+// }
+// =============================================================================
+
+
+
+
