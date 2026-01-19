a5ae42f13b08bec6b3d0aed8c199984fba2ea8e1
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2026-01-19 11:10:20 +0530

    commit

--- java_script/Basics/events/event.js
+++ java_script/Basics/events/event.js
@@ -14,10 +14,59 @@
 // }
 
 // ! ---------3rd way---------.
-document.getElementById("btn").addEventListener("click",function info(){
-    alert("I am using addEventListener event")
-},false)
-document.getElementById("btn").addEventListener("click",info,false)
+// document.getElementById("btn").addEventListener("click",function info(){
+//     alert("I am using addEventListener event")
+// },false)
+// document.getElementById("btn").addEventListener("click",info,false)
+
+
+
+//! dbclick:
+// document.getElementById("btn").addEventListener("dblclick",function info(){
+//     alert("I am using dblclick event")
+// },false)
+
+//! Mouseover:
+
+// document.getElementById("btn").addEventListener("mouseover",function info(){
+//     alert("I am using mouseover event")
+// },false)
+
+//! Mouseout:
+
+// document.getElementById("btn").addEventListener("mouseout",function info(){
+//     alert("I am using mouseout event")
+// },false)
+
+
+//! mousemove:
+
+// document.getElementById("btn").addEventListener("mousemove",function info(){
+//     alert("I am using mousemove event")
+// },false)
+
+//! mouseenter:
+
+// document.getElementById("btn").addEventListener("mouseenter",function info(){
+//     alert("I am using mouseneter event")
+// },false)
+
+
+//! mouseleave:
+
+// document.getElementById("btn").addEventListener("mouseleave",function info(){
+//     alert("I am using mouseleave event")
+// },false)
+
+document.getElementById("btn").onclick = function info(e){
+    console.log(e.clientX)
+    console.log(e.clientY)
+    console.log(e.target)
+    console.log(e)
+
+
+    console.log("I am using onclik event")
+}
 
 
 
