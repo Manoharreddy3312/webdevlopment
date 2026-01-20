24a095add237801bf424adbba204304fc52d5943
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2026-01-20 10:33:37 +0530

    commit

--- java_script/Basics/events/event.js
+++ java_script/Basics/events/event.js
@@ -11,7 +11,9 @@
 
 // function info(){
 //     alert("I am using inline event")
-// }
+// }.
+
+// ==========================================================================================================
 
 // ! ---------3rd way---------.
 // document.getElementById("btn").addEventListener("click",function info(){
@@ -58,15 +60,141 @@
 //     alert("I am using mouseleave event")
 // },false)
 
-document.getElementById("btn").onclick = function info(e){
-    console.log(e.clientX)
-    console.log(e.clientY)
-    console.log(e.target)
-    console.log(e)
+// document.getElementById("btn").onclick = function info(e){
+//     console.log(e.clientX)
+//     console.log(e.clientY)
+//     console.log(e.target)
+//     console.log(e)
+
+//     console.log("I am using onclik event")
+// }
+
+// ==========================================================================================================
 
 
-    console.log("I am using onclik event")
-}
+//!   KEYBOARD EVENTS
+
+// let user  = document.getElementById("username")
+// user.onkeypress = function info(){
+//     alert("I am using onkeypress event")
+// }
+
+// user.onkeydown = function info(){
+//     alert("I am using onkeydown event")
+// }
+
+// user.onkeyup = function info(){
+//     alert("I am using onkeyup event")
+// }
+
+// user.onblur = function info(){
+//     alert("I am using onblur event")
+// }
+
+// user.onfocus = function info(){
+//     alert("I am using onfocus event")   
+// }
+// user.addEventListener("keypress",function info(e)){
+//     console.log(e.target.value)
+// }
+
+// ==========================================================================================================
+
+
+// ! FORM EVENTS
+
+// user.onsubmit = function info(){
+//     alert("I am using onsubmit event")
+// }
+
+// user.onreset = function info(){
+//     alert("I am using onreset event")
+// }
+
+// user.oninput = function info(){
+//     alert("I am using oninput event")
+// }
+
+// user.oninvalid = function info(){
+//     alert("I am using oninvalid event")
+// }
+// user.focus = function info(){
+//     alert("I am using onfocus event")
+// }
+
+// user.blur = function info(){
+//     alert("I am using onblur event")
+// }
+
+// user.addEventListener("input",function info(e){
+//     console.log(e.target.value)
+// })
+
+// user.addEventListener("change",function info(e){
+//     console.log(e.target.value)
+// })
+
+// user.addEventListener("blur",function info(e){
+//     console.log(e.target.value,"blurevent")
+// })
+// user.addEventListener("focus",function info(e){
+//     console.log(e.target.value,"focusevent")
+// })
+
+// ==========================================================================================================
+
+
+//! POINTER EVENT :
+
+// document.getElementById("btn").addEventListener("pointerdown",function info(){
+//     console.log("I am using pointerdown event")
+// },false)
+
+// document.getElementById("btn").addEventListener("pointerup",function info(){
+//     console.log("I am using pointerup event")
+// },false)
+
+// document.getElementById("btn").addEventListener("pointerover",function info(){
+//     console.log("I am using pointerover event")
+// },false)
+
+// document.getElementById("btn").addEventListener("pointerout",function info(){
+//     console.log("I am using pointerout event")
+// },false)
+
+// document.getElementById("btn").addEventListener("pointerenter",function info(){
+//     console.log("I am using pointerenter event")
+// },false)
+
+// document.getElementById("btn").addEventListener("pointerleave",function info(){
+//     console.log("I am using pointerleave event")
+// },false)
+
+// document.getElementById("btn").addEventListener("pointermove",function info(){
+//     console.log("I am using pointermove event")
+// },false)
+
+// ==========================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
