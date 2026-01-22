33aec7287508ee3ac2ce144e09c522e12c982260
Manohar Reddy Mosarla <mosarlamanoharreddy2@gmail.com>
2026-01-22 10:14:26 +0530

    your message

--- java_script/Basics/module/module.md
+++ java_script/Basics/module/module.md
@@ -1,2 +1,13 @@
+MODULES :
+========
+
+These are 2 types:
+
+1.import
+2.export
+
+1.import:
+========
+named function  or default name
 
 
---
+++ java_script/Basics/module/module1.js
@@ -0,0 +1,11 @@
+import a from "./module.js"
+console.log(a)
+a(10,20)
+
+
+import {sub} from "./module.js"
+sub(10,20)
+
+import {mul} from "./module.js"
+mul(10,20)
+
