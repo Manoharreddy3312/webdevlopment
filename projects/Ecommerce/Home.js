a4776430726ce67597aad368c1dcf94e25f20713
unknown <mosarlamanoharreddy2@gmail.com>
2026-01-27 20:25:13 +0530

    pushing files

---
+++ projects/Ecommerce/Home.js
@@ -0,0 +1,58 @@
+
+// ---------Fetching API----------
+
+let product = [];
+
+function fetchData() {
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((val) => {
+      product = val.products;
+      console.log(product);
+      localStorage.setItem("allProducts", JSON.stringify(product));
+      displayProducts(product);
+    })
+    .catch((err) => console.error(err));
+}
+
+// -------display Products----------
+
+function displayProducts(prod) {
+  console.log(prod, "displayProducts")
+  let output = "";
+  prod.map((val) => {
+    output += `
+  <main>
+  <div class="image">
+  <img src="${val.thumbnail}">
+  </div>
+  <h3>${val.title}</h3>
+  <div class="info">
+    <span class="rating">${val.rating} ★</span>
+    <p class="price">₹${(val.price * 83).toFixed(2)}</p>
+  </div>
+  <div class="actions">
+    <p class="stock">Stock: ${val.stock}</p>
+    <button>Details</button>
+  </div>
+
+  </main>
+
+  `
+  })
+  document.getElementById("product_container").innerHTML = output;
+}
+fetchData();
+
+// ------- SERCH ITEMS ---------
+
+document.getElementById("search_bar").addEventListener("input", function searchItems(e) {
+  let searchTerm = e.target.value.toLowerCase();
+  let filteredProduct = product.filter((v) => {
+    return v.title.toLowerCase().includes(searchTerm) ||
+      v.category.toLowerCase().includes(searchTerm.toLowerCase());
+  })
+  displayProducts(filteredProduct);
+})
+
+
